33a6ba18d1c96e3447cab50a84366395948d7551
Brandon Michael Piper <bmp713@yahoo.com>
2018-06-24 22:55:57 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -15,14 +15,12 @@
         </div>
 
         <div id="content">
-            
             <div id="content_1">
                 <h1>PHP | PDO and Restful API</h1><br>
                 <h3>Log in to database</h3><br><br>
                 <form action="index.php" method="POST" id="form_login">
                     User<br><input type="text" name="user" value="root" class="input-box"><br>
                     Password<br><input type="password" name="password" value="Welcome123" class="input-box"><br><br>
-
                     First<br><input type="text" name="first" value="First" class="input-box"><br>
                     Last<br><input type="text" name="last" value="Last" class="input-box"><br>
                     User Name<br><input type="text" name="uid" value="User" class="input-box"><br>
@@ -31,12 +29,14 @@
    				</form><br>
                 
             </div>	
+
             <div id="output">
                 <?php
 
                     $db_file = 'db.sql';                
                     $host = 'localhost';
                     $db_name = 'PDO_1';
+
                     $username = $_POST['user'];
                     $password = $_POST['password'];
 
@@ -49,41 +49,47 @@
                         echo 'POST = '.$post.'<br>';
                     }
                     // Create database from SQL file
-                    /*if( file_exists( $db_file ) ){
+                    if( file_exists( $db_file ) ){
                         $sql = file( $db_file );
                         foreach( $sql as $line ){
                             echo $line.'<br>';
                         }
-                        $cmd = 'mysql -u'.$db_user.' -p'.$db_password.' <'.$db_file;
-                        echo '<br>'.$cmd.'<br><br>';
+                        $cmd = 'mysql -u'.$username.' -p'.$password.' <'.$db_file;
+                        echo $cmd.'<br>';
                         echo exec( $cmd );
-                    }*/
+                    }
 
                     // CONNECTION
                     try{ 
                         $conn = new PDO('mysql:host='.$host.';dbname='.$db_name, $username, $password);
                         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                        printf("<br>%s logged in with %s<br>", $username, $password);
+                        $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+                      
+                        printf("<br><br>%s logged in with %s<br>", $username, $password);
+                                    
+                        // CREATE
+                        $sql = "INSERT INTO users_1 (first, last, uid, age) 
+                            VALUES ('$first', '$last', '$uid', $age);";
+                        
+                        if( $conn->exec( $sql ) ){
+                            foreach( $_POST as $post ){
+                                echo 'POST = '.$post.'<br>';
+                            }
+                            echo "<br>Record created successfully";
+                        }
+                        // READ
+                        echo '<br>READ<br>';
+
+                        $sql = "SELECT * FROM users_1;";
+                        $result = $conn->exec( $sql );
+
+                        while( $row = $result->fetch( PDO::FETCH_ASSOC ) ){
+                            printf("ROW = %s<br>", print_r($row) );
+                        }
+
                     }
                     catch( PDOException $error ){
                         echo 'Connection Error: ' . $error->getMessage();
-                    }
-                    $conn = null;
-
-                    // CREATE RECORD
-                    try{ 
-                        $conn = new PDO('mysql:host='.$host.';dbname='.$db_name, $username, $password);
-                        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                        printf("<br>%s logged in with %s<br>", $username, $password);
-
-                        $query = "INSERT INTO users_1 (first, last, uid, age) 
-                            VALUES ('$first', '$last', '$uid', $age);";
-                        
-                        $conn->exec( $query );
-                        echo "Record created successfully";
-                    }
-                    catch(PDOException $error){
-                        echo $sql . "<br>" . $error->getMessage();
                     }
                     $conn = null;
                                   
