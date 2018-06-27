9dae5a3f7d811745e8eb948b9f2f4df6023500dd
Brandon Michael Piper <bmp713@yahoo.com>
2018-06-26 19:23:14 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -16,115 +16,127 @@
 
         <div id="content">
             <div id="content_1">
-                <h1>PHP | PDO Restful API</h1>
-                <h2>Useful MySQL Queries</h2><br><br>
-                <h3>Log in to database</h3><br>
+                <h1>PDO | Restful API</h1>
                 <form action="index.php" method="POST" id="form_login">
-                    User<br><input type="text" name="user" value="root" class="input-box"><br>
-                    Password<br><input type="password" name="password" value="Welcome123" class="input-box"><br><br><br>
-
-                    <h3>Enter user name,<br> 
-                        create new or update existing</h3><br>
+                    <h5>Enter user name, create, read, update, or delete existing account</h5><br>
                     User Name<br><input type="text" name="uid" value="first" placeholder="User Name" class="input-box"><br>
                     First<br><input type="text" name="first" value="First" placeholder="First Name" class="input-box"><br>
                     Last<br><input type="text" name="last" value="Last" placeholder="Last Name" class="input-box"><br>
-                    Age<br><input type="text" name="age" value="0" placeholder="Age" class="input-box"><br><br>
-                    <button type="submit" class="main-button">Update</button>
+                    Age<br><input type="text" name="age" value="0" placeholder="Age" class="input-box"><br><br><br>
+                    <button type="submit" name="Create" class="main-button">Create</button>
+                    <button type="submit" name="Read" class="main-button">Read</button>
+                    <button type="submit" name="Update" class="main-button">Update</button>
+                    <button type="submit" name="Delete" class="main-button">Delete</button>
+                    <br><br>Clear to default users<br>
+                    <button type="submit" name="Reset" class="main-button">Reset</button>
                 </form><br>
             </div>	
             <div id="output">
                 <?php
-                    $db_file = 'db.sql';                
+                    $db_file = 'db_default.sql';                
                     $host = 'localhost';
                     $db_name = 'PDO_1';
 
-                    $username = $_POST['user'];
-                    $password = $_POST['password'];
+                    //$username = $_POST['user'];
+                    //$password = $_POST['password'];
+                    $username = 'root';
+                    $password = 'Welcome123';
 
                     $first = $_POST['first'];
                     $last = $_POST['last'];
                     $uid = $_POST['uid'];
                     $age = $_POST['age'];
 
-                    // Create database from SQL file
-                    if( file_exists( $db_file ) ){
-                        $sql = file( $db_file );
-                        foreach( $sql as $line ){
-                            echo $line.'<br>';
-                        }
-                        $cmd = 'mysql -u'.$username.' -p'.$password.' <'.$db_file;
-                        echo $cmd.'<br>';
-                        echo exec( $cmd );
-                    }
+                    // Create database from SQL file if reset button clicked
+                    if( isset($_POST['Reset']) )
 
-                    echo "<br>// Display POST data<br>";
+                        if( file_exists( $db_file ) ){
+                            $sql = file( $db_file );
+                            foreach( $sql as $line ){
+                                //echo $line.'<br>';
+                            }
+                            $cmd = 'mysql -u'.$username.' -p'.$password.' <'.$db_file;
+                            echo $cmd.'<br>';
+                            echo exec( $cmd );
+                        }
+
+                    echo "<br><br>// Display POST data<br>";
                     foreach( $_POST as $post ){
                         echo 'POST = '.$post.'<br>';
                     }
 
-
                     
                     // CONNECTION
                     try{ 
-                        echo "// Restful API CRUD operations with PDO<br><br>";
+                        // Restful API CRUD operations with PDO
 
                         $conn = new PDO('mysql:host='.$host.';dbname='.$db_name, $username, $password);
                         $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
                         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             
                         printf("<br><br>%s logged in with %s<br>", $username, $password);
-                                 
-                        
+
                         // CREATE
-                        echo "<br>// CREATE<br>";
+                        if( isset($_POST['Create']) ){
 
-                        $sql = "INSERT INTO users_1 (first, last, uid, age) 
-                            VALUES ('$first', '$last', '$uid', $age);";
-                        
-                        if( $conn->exec( $sql ) ){
-                            foreach( $_POST as $post ){
-                                echo 'POST = '.$post.'<br>';
+                            echo "<br>// CREATE<br>";
+
+                            $sql = "INSERT INTO users_1 (first, last, uid, age) 
+                                VALUES ('$first', '$last', '$uid', $age);";
+                            
+                            if( $conn->exec( $sql ) ){
+                                foreach( $_POST as $post ){
+                                    echo 'POST = '.$post.'<br>';
+                                }
+                                echo "<br>//Record created successfully<br>";
+
                             }
-                            echo "<br>//Record created successfully<br>";
-
                         }
-
 
                         // READ
-                        echo '<br>// READ<br>';
-                        
-                        // Fetch PDO data with associative array
-                        $result = $conn->query( "SELECT * FROM users_1" );
-                        while( $row = $result->fetch( PDO::FETCH_ASSOC ) ){
-                            echo print_r($row).'<br>';
-                        }
+                        if( isset($_POST['Read']) ){
 
+                            echo '<br>// READ<br>';
+
+                            // Fetch PDO data with associative array
+                            $result = $conn->query( "SELECT * FROM users_1" );
+                            while( $row = $result->fetch( PDO::FETCH_ASSOC ) ){
+                                echo print_r($row).'<br>';
+                            }
+                        }
 
                         // UPDATE
-                        echo "<br>// UPDATE<br>";
+                        if( isset($_POST['Update']) ){
 
-                        $query =  "UPDATE users_1 SET age=".$_POST['age']." WHERE uid='".$_POST['uid']."'";
-                        echo "QUERY = $query<br>";
-                        $result = $conn->query( $query );
-                        
-                        echo "<br>// Print updated results<br<br><br><br>";
-                        $result = $conn->query( "SELECT * FROM users_1" );
-                        while( $row = $result->fetch( PDO::FETCH_ASSOC ) ){
-                            echo print_r($row).'<br>';
+                            echo "<br>// UPDATE<br>";
+
+                            $query =  "UPDATE users_1 SET age=".$_POST['age']." WHERE uid='".$_POST['uid']."'";
+                            echo "QUERY = $query<br>";
+                            $result = $conn->query( $query );
+                            
+                            echo "<br>// Print updated results<br<br><br>";
+                            $result = $conn->query( "SELECT * FROM users_1" );
+                            while( $row = $result->fetch( PDO::FETCH_ASSOC ) ){
+                                echo print_r($row).'<br>';
+                            }
                         }
 
-
                         // DELETE
-                        echo "<br>// DELETE<br>";
+                        if( isset($_POST['Delete']) ){
+                            // Reset delete flag to false;
+                            $_POST['Delete'] = 0;
 
-                        $query =  "DELETE FROM users_1 WHERE uid='".$_POST['uid']."'"; 
-                        echo "QUERY = $query<br>";
-                        $result = $conn->query( $query );
-                        
-                        echo "<br>// Print updated results<br<br><br><br>";
-                        $result = $conn->query( "SELECT * FROM users_1" );
-                        while( $row = $result->fetch( PDO::FETCH_ASSOC ) ){
-                            echo print_r($row).'<br>';
+                            echo "<br>// DELETE<br>";
+
+                            $query =  "DELETE FROM users_1 WHERE uid='".$_POST['uid']."'"; 
+                            echo "QUERY = $query<br>";
+                            $result = $conn->query( $query );
+                            
+                            echo "<br>// Print updated results<br<br><br>";
+                            $result = $conn->query( "SELECT * FROM users_1" );
+                            while( $row = $result->fetch( PDO::FETCH_ASSOC ) ){
+                                echo print_r($row).'<br>';
+                            }
                         }
 
                     }
